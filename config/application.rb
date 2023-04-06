3cb2b469074f18225caff7d10bb47ecf0a1fd704
Alina Pisarenko <pisarenko.alina@gmail.com>
2023-04-06 16:34:02 -0400

    test

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,5 @@ module Phase4Project
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.active_support.key_generator_hash_digest_class = OpenSSL::Digest::SHA1
   end
 end
